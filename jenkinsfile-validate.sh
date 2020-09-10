d75dbe8ea3f1e9938332aa060f60b6fb1a8423c1
Luan Tran <minhluantran017@gmail.com>
2020-09-10 21:02:09 +0700

    jenkins-shell: sync variables convention

--- jenkinsfile-validate.sh
+++ jenkinsfile-validate.sh
@@ -6,13 +6,14 @@ function get_jenkins_cred() {
     sed -nr "/^\[$l_profile\]/ { :l /^$l_item[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $HOME/.jenkins/config
 }
 
+JENKINS_FILE=$1
+JENKINS_PROFILE=$2
 JENKINS_URL=${JENKINS_URL:=$(get_jenkins_cred JENKINS_URL)}
 JENKINS_USER=${JENKINS_USER:=$(get_jenkins_cred JENKINS_USER)}
-JENKINS_API_TOKEN=${JENKINS_API_TOKEN:=$(get_jenkins_cred JENKINS_API_TOKEN)}
-JENKINS_FILE=$1
+JENKINS_API_TOKEN=${JENKINS_API_TOKEN:=$(get_jenkins_cred JENKINS_API_TOKEN ${JENKINS_PROFILE})}
 
 echo "Using ${JENKINS_URL} with user ${JENKINS_USER}"
 echo "Getting Jenkins crumb hex..."
-JENKINS_CRUMB=`curl -sS -u$JENKINS_USER:$JENKINS_API_TOKEN "$JENKINS_URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
-echo "Validating Jenkinsfile $JENKINS_FILE ..."
-curl -X POST -u$JENKINS_USER:$JENKINS_API_TOKEN -H $JENKINS_CRUMB -F "jenkinsfile=<$JENKINS_FILE" $JENKINS_URL/pipeline-model-converter/validate
+JENKINS_CRUMB=`curl -sS -u${JENKINS_USER}:${JENKINS_API_TOKEN} "${JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)"`
+echo "Validating Jenkinsfile ${JENKINS_FILE} ..."
+curl -X POST -u${JENKINS_USER}:${JENKINS_API_TOKEN} -H ${JENKINS_CRUMB} -F "jenkinsfile=<${JENKINS_FILE}" ${JENKINS_URL}/pipeline-model-converter/validate
